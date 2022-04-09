7382d791767de0db303ca8de037979f727ad9a81
Gopinath Swaminathan <gopinathswaminadane@gmail.com>
2022-04-09 12:51:06 -0700

    Updated with better approach

--- src/day1/SetMatrixZero.java
+++ src/day1/SetMatrixZero.java
@@ -22,11 +22,11 @@ package day1;
 import java.util.Arrays;
 
 public class SetMatrixZero {
-//Simple approach
+    //Simple approach
     void setMatrix(int[][] matrix) {
         int rows = matrix.length, cols = matrix[0].length;
-        int dummy1[] = new int[rows];
-        int dummy2[] = new int[cols];
+        int[] dummy1 = new int[rows];
+        int[] dummy2 = new int[cols];
         Arrays.fill(dummy1, -1);
         Arrays.fill(dummy2, -1);
         for (int i = 0; i < rows; i++) {
@@ -49,16 +49,29 @@ public class SetMatrixZero {
 
     //better approach
 
-    void setNewMatrix(int [][]input)
-    {
-        
+    static void setZeroes(int[][] matrix) {
+        int col0 = 1, rows = matrix.length, cols = matrix[0].length;
+
+        for (int i = 0; i < rows; i++) {
+            if (matrix[i][0] == 0) col0 = 0;
+            for (int j = 1; j < cols; j++)
+                if (matrix[i][j] == 0)
+                    matrix[i][0] = matrix[0][j] = 0;
+        }
+
+        for (int i = rows - 1; i >= 0; i--) {
+            for (int j = cols - 1; j >= 1; j--)
+                if (matrix[i][0] == 0 || matrix[0][j] == 0)
+                    matrix[i][j] = 0;
+            if (col0 == 0) matrix[i][0] = 0;
+        }
     }
 
 
     public static void main(String[] args) {
         SetMatrixZero smz = new SetMatrixZero();
         int arr[][] = {{0, 1, 2, 0}, {3, 4, 5, 2}, {1, 3, 1, 5}};
-        smz.setMatrix(arr);
+        setZeroes(arr);
         for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < arr[0].length; j++) {
                 System.out.print(arr[i][j] + " ");
