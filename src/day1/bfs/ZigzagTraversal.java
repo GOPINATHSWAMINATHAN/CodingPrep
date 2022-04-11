93b105a8242868a081bf234e4162c658f5cd62de
Gopinath Swaminathan <gopinathswaminadane@gmail.com>
2022-04-11 13:03:20 -0700

    Added ZigzagTraversal order traversal

---
+++ src/day1/bfs/ZigzagTraversal.java
@@ -0,0 +1,20 @@
+package day1.bfs;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class ZigzagTraversal {
+
+    static List<List<Integer>> zigZagTraverse(TreeNode root) {
+
+        List<List<Integer>> result = new ArrayList<>();
+
+        Queue<TreeNode> queue=new LinkedList<>();
+        
+
+        return result;
+    }
+
+}
